111a4b7f770ad05353501feb1e4ba9e379152809
10-ui <ui-10@outlook.jp>
2024-05-23 19:15:48 +0900

    レイアウトを日本語対応し、フォントを別ファイルへ

---
+++ lib/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Noto_Sans_JP } from 'next/font/google';
+const inter = Inter({ subsets: ['latin'] });
+const notoSansJP = Noto_Sans_JP({ subsets: ['latin'] });
+
+export { inter, notoSansJP };
