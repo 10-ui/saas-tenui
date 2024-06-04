a8f3513dbb7e909b46a5c0f6ffa96d540ebdcdd9
10-ui <ui-10@outlook.jp>
2024-06-04 18:21:59 +0900

    ActiveClassを追加。

--- components/header.tsx
+++ components/header.tsx
@@ -1,9 +1,15 @@
+"use client";
 import Link from "next/link";
 import Image from "next/image";
 import favicon from "@/public/favicon.svg";
 import { Button } from "@/components/ui/button";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
+  const pathname = usePathname();
+  const isActive = (path: string) => pathname === path;
+  const activeClass = (path: string) =>
+    isActive(path) ? "text-slate-900" : "";
   return (
     <header className='flex items-center justify-between px-9 py-6'>
       <div className='flex items-center gap-6'>
@@ -15,17 +21,26 @@ export default function Header() {
         </h1>
         <nav className='flex gap-3'>
           <Button variant={"hover"} asChild>
-            <Link href='#'>Features</Link>
+            <Link href='/features' className={`${activeClass("/features")}`}>
+              Features
+            </Link>
           </Button>
           <Button variant={"hover"} asChild>
-            <Link href='/pricing'>Pricing</Link>
+            <Link href='/pricing' className={`${activeClass("/pricing")}`}>
+              Pricing
+            </Link>
           </Button>
           <Button variant={"hover"} asChild>
-            <Link href='#'>Docs</Link>
+            <Link href='/docs' className={`${activeClass("/docs")}`}>
+              Docs
+            </Link>
           </Button>
         </nav>
       </div>
-      <Button variant={"outline"}>Login</Button>
+      <div className='flex gap-3'>
+        <Button variant={"outline"}>Login</Button>
+        <Button variant={"default"}>Sign Up</Button>
+      </div>
     </header>
   );
 }
